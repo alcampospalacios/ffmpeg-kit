08c53c8a51a4b5bc2df58a46c2b4928d567fbe6c
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2026-01-16 03:20:09 +0000

    feat: new version

--- Package.swift
+++ Package.swift
@@ -24,43 +24,43 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ffmpegkit",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/ffmpegkit.xcframework.zip",
-            checksum: "de0affab5cc9b0413b88fa5846098d0ab439362a561fa6478093071008067e3b"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/ffmpegkit.xcframework.zip",
+            checksum: "7cc6873a5cccf2140e94ea388c83afa613164028e19d03d212421cf23792b2e4"
         ),
         .binaryTarget(
             name: "libavcodec",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libavcodec.xcframework.zip",
-            checksum: "0593523e376481cef1a5d3e6f8c9fe272f1dcc902382a587fac81b1691d616a7"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libavcodec.xcframework.zip",
+            checksum: "6d9f63a287a5a16ae693f6d927c84732ec7e58e69ba0fd440dd72180e32e9a65"
         ),
         .binaryTarget(
             name: "libavdevice",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libavdevice.xcframework.zip",
-            checksum: "c249d2733b1316190c6e4b6f0c0c495bb7663c5c22a2acdf706df9ca61c4571f"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libavdevice.xcframework.zip",
+            checksum: "e89df29ebfc2c2c5ecc089def18175755d55b8b6fdb6c0fcbd25ea42a34f80e2"
         ),
         .binaryTarget(
             name: "libavfilter",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libavfilter.xcframework.zip",
-            checksum: "aee2465e61c4d476a9625d8ee448f01410cb0e2819be48f2858bfe2fff076222"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libavfilter.xcframework.zip",
+            checksum: "64a47370afe006ab531410f4c555d8f98ce735c18c48356ee9cdf363c4cdffda"
         ),
         .binaryTarget(
             name: "libavformat",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libavformat.xcframework.zip",
-            checksum: "4b61617e2e5551d255d9512c0d248154f2a644f4519d27bf62776cbaa185a49d"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libavformat.xcframework.zip",
+            checksum: "498eba93fa19f957eadc33457ec5fdcd4da86178e057066736a041e6e953939c"
         ),
         .binaryTarget(
             name: "libavutil",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libavutil.xcframework.zip",
-            checksum: "1856d813f64fb8c81d23eb18aa539973006b92acedfcbbe039b49a64557342a5"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libavutil.xcframework.zip",
+            checksum: "84e59530bd615dbf3d2733a0ebbc5614ccbe9e0116542a40d2b78ceeb6c7e060"
         ),
         .binaryTarget(
             name: "libswresample",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libswresample.xcframework.zip",
-            checksum: "32fec2f646376789da78fcf3406d3089d029bdd646005bdd5a17b4e300c14ab3"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libswresample.xcframework.zip",
+            checksum: "1301934d9a5640af0dff9146b9672dc2338f4539ef66b2931f764757fddfe887"
         ),
         .binaryTarget(
             name: "libswscale",
-            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.1-custom-build/libswscale.xcframework.zip",
-            checksum: "1c6654a6bf1d9a61fdf842fb07f720aa406f30d775cd274f4a4e56f768a9c492"
+            url: "https://github.com/alcampospalacios/ffmpeg-kit/releases/download/v6.0.2-custom-build/libswscale.xcframework.zip",
+            checksum: "7e57f9470fc43640b976482a25821d844312ee8b758ff855369d0f99cf3a741f"
         )
     ]
 )
